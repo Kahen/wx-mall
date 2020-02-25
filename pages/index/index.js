bb158a9fa1bd3b513206ca0e86bc9cb7cf65ca88
Kahen <KahenLee@163.com>
2020-02-25 17:18:02 +0800

    楼层

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,9 +7,11 @@ Page({
     // 轮播图数组
     swiperList: [],
     // 导航图数组
-    catesList:[]
+    catesList: [],
+    // 楼层数据
+    FloorList: []
   },
-  // 页面开始加载 就会触发
+  // 页面开始加载 就会触发 
   onLoad: function (options) {
     // 1 发送异步请求获取轮播图数据
     // wx.request({
@@ -22,6 +24,7 @@ Page({
     // });
     this.getSwiperList();
     this.getCatesList();
+    this.getFloorList();
   },
   // 获取轮播图数据
   getSwiperList() {
@@ -42,6 +45,17 @@ Page({
         this.setData({
           catesList: result
         })
-      }) 
+      })
+  },
+  // 获取 楼层数据
+  getFloorList() {
+    request({
+        url: "/home/floordata"
+      })
+      .then(result => {
+        this.setData({
+          floorList: result
+        })
+      })
   }
 });
