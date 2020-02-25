079fb22490a9c2902c337f69f96c8c2951b6cc7f
Kahen <KahenLee@163.com>
2020-02-25 15:02:26 +0800

    导航图
    轮播图导航图和楼层都使用了promise技术

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,47 @@
-//Page Object
+// 0 引入 用来发送请求的 方法 一定要把补全
+import {
+  request
+} from "../../request/index.js";
 Page({
   data: {
     // 轮播图数组
-    swiperList:[]
+    swiperList: [],
+    // 导航图数组
+    catesList:[]
   },
   // 页面开始加载 就会触发
-  onLoad: function(options) {
+  onLoad: function (options) {
     // 1 发送异步请求获取轮播图数据
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-      success: (result) => {
-        console.log(result);
-      }
-    });
+    // wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     })
+    //   }
+    // });
+    this.getSwiperList();
+    this.getCatesList();
   },
+  // 获取轮播图数据
+  getSwiperList() {
+    request({
+        url: "/home/swiperdata"
+      })
+      .then(result => {
+        this.setData({
+          swiperList: result
+        })
+      })
+  },
+  getCatesList() {
+    request({
+        url: "/home/catitems"
+      })
+      .then(result => {
+        this.setData({
+          catesList: result
+        })
+      }) 
+  }
 });
-  
