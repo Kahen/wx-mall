35a4a0789493de6bed85cac01d6dbf6752d4a85e
Kahen <KahenLee@163.com>
2020-02-29 16:13:12 +0800

    分类

--- request/index.js
+++ request/index.js
@@ -19,7 +19,7 @@ export const request=(params)=>{
     
 
   // 定义公共的url
-  const baseUrl="https://api.zbztb.cn/api/public/v1";
+  const baseUrl="https://api-hmugo-web.itheima.net/api/public/v1";
   return new Promise((resolve,reject)=>{
     wx.request({
      ...params,
